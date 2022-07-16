751482d90ef3f4d533312421b882d714b4f01280
CarlosGuzmanCG <cg546289@gmail.com>
2022-07-16 17:52:03 -0500

    IMC-Btn

---
+++ Btn/Btn/Btn/Vistas/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Btn.Vistas
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSecun_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PrimeraPag());
+        }
+
+        private void btnHola_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Btn Hola","Hola","OK");
+        }
+
+        private void btnCalc_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtN1.Text) && !string.IsNullOrEmpty(txtN2.Text))
+            {
+                calcu();
+            }
+            else
+            {
+                DisplayAlert("Datos incorrectos","Verificar","OK");
+            }
+        }
+
+        private void calcu()
+        {
+            double v1 = Convert.ToDouble(txtN1.Text);
+            double v2 = Convert.ToDouble(txtN2.Text);
+
+            double resultado = v1 + v2;
+
+            DisplayAlert("Resultado", "R=" + resultado, "OK");
+            txtN1.Text = "";
+            txtN2.Text = "";
+        }
+    }
+}
---
+++ IMC/IndiceMC/IndiceMC/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IndiceMC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPeso.Text) && !string.IsNullOrEmpty(txtAltura.Text))
+            {
+                calcularIMC();
+            }
+            else
+            {
+                DisplayAlert("AVISO!!", "Favor de ingresar los valores", "OK");
+            }
+
+        }
+
+        private void calcularIMC()
+        {
+            double altura = Convert.ToDouble(txtAltura.Text);
+            double peso = Convert.ToDouble(txtPeso.Text);
+            if (altura >0 && peso>0)
+            {
+                double resultado = peso / Math.Pow(altura, 2);
+                string mensaje = "";
+
+                if (resultado < 18.5)
+                {
+                    mensaje = "Tienes bajo peso!! \n Resultado:" + resultado;
+                }
+                else if (resultado >= 18.5 && resultado <= 24.9)
+                {
+                    mensaje = "Tu peso es normal! \n Resultado:" + resultado;
+                }
+                else
+                {
+                    mensaje = "Tienes sobrepeso 😥 \n Resultado:" + resultado;
+                }
+                DisplayAlert("Resultado", mensaje, "OK");
+            }
+            else
+            {
+                DisplayAlert("AVISO!!","Datos incorrectos","OK");
+            }
+            txtAltura.Text = "";
+            txtPeso.Text = "";
+        }
+    }
+}
