795fa801c582f7c2ac888960726d8527d4a73b8f
CarlosGuzmanCG <cg546289@gmail.com>
2022-07-16 22:07:46 -0500

    Componentes1

---
+++ Componentes/Componentes/Componentes/Modelo/Mcomidas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Componentes.Modelo
+{
+    public class Mcomidas
+    {
+        public string nombre { get; set;}
+        public string precio { get; set; }
+    }
+}
---
+++ Componentes/Componentes/Componentes/Vistas/MenuInicial.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Componentes.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Componentes.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuInicial : ContentPage
+    {
+        public IList<Mcomidas> Comidas { get; private set;} 
+        public MenuInicial()
+        {
+            InitializeComponent();
+            Comidas = new List<Mcomidas>();
+            Comidas.Add(new Mcomidas
+            { 
+                nombre="Tortas",
+                precio="$ 14"
+            });
+
+            Comidas.Add(new Mcomidas
+            {
+                nombre = "Tacos",
+                precio = "$ 19"
+            });
+
+            Comidas.Add(new Mcomidas
+            {
+                nombre = "Tortilla",
+                precio = "$ 16"
+            });
+
+
+            BindingContext = this;
+        }
+
+
+    }
+}
