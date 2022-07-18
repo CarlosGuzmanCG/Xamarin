865bfa68f28f9c96c24ebe557dea5bb6237605e5
CarlosGuzmanCG <cg546289@gmail.com>
2022-07-17 22:25:44 -0500

    ListV

---
+++ ListView/ListView/ListView/Modelo/ListV.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListView.Modelo
+{
+    public class ListV
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+    }
+}
---
+++ ListView/ListView/ListView/Vistas/Mprincipal.xaml.cs
@@ -0,0 +1,51 @@
+﻿using ListView.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListView.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Mprincipal : ContentPage
+    {
+        public IList<ListV> Datos { get; private set; }
+        public Mprincipal()
+        {
+            InitializeComponent();
+
+            Datos = new List<ListV>();
+
+            Datos.Add(new ListV
+            { 
+                nombre = "Jose",
+                apellido = "pedro"
+            });
+
+            Datos.Add(new ListV
+            {
+                nombre = "Carlos",
+                apellido = "pedro"
+            });
+
+            Datos.Add(new ListV
+            {
+                nombre = "Ale",
+                apellido = "pedro"
+            });
+
+            Datos.Add(new ListV
+            {
+                nombre = "Pablo",
+                apellido = "pedro"
+            });
+
+            BindingContext = this;
+
+        }
+    }
+}
